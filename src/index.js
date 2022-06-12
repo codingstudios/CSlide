dd63ad075da81b176717c3c605db22b2d41b12fe
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-06-12 22:02:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 #! /usr/bin/env node
 const slideFile = process.argv[2];
+const slideHTMLFile = process.argv[3];
+
 if (!slideFile) throw new Error('CSlide: [Error] Please provide a markdown filepath.');
+if (!slideHTMLFile) throw new Error('CSlide: [Error] Please provide a HTML filename for this slide.');
+
 const fs = require('fs');
 if(!fs.existsSync(slideFile)) throw new Error("CSlide: [Error] Invalid File !");
 const types = {
@@ -9,7 +13,8 @@ const types = {
     ["-"]: "<li>{value}</li>",
 };
 
-console.log(`CSlide: [Watching] ${slideFile}`)
+console.log(`CSlide: [Watching] ${slideFile} - (Writing) ${slideHTMLFile}.html`);
+
 fs.watch(slideFile, { encoding: 'utf-8' }, (eventType) => {
 if (eventType !== 'change') return;
 
@@ -17,8 +22,8 @@ const slide = fs.readFileSync(slideFile, 'utf-8');
 const slides = slide.split('-----');
 var html = ``;
 
-slides.forEach((page) => {
-        var div = `<div>`;
+slides.forEach((page, index) => {
+        var div = `<div id="${index}">`;
         var line = page.split('\n');
         line.forEach((text) => {
             var value = text.split(" ");
@@ -34,8 +39,8 @@ slides.forEach((page) => {
         html += `${div}`;
 })
     
-const finalHTML = `<link rel="stylesheet" href="style.css"><body><div class="container">${html}</div></body>`;
+const finalHTML = `<link rel="stylesheet" href="slide.css"><body><div class="container" id="slide">${html}</div></body><script src="script.js"></script>`;
     
-fs.writeFileSync(`./slide.html`, finalHTML); 
+fs.writeFileSync(`./${slideHTMLFile}.html`, finalHTML); 
 console.log(`CSlide: [Merged] (${slideFile}) => ./slide.html`)
 })
